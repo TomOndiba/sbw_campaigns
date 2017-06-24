74dff1b17b789bcb56b2b8ba7db567f775aea4c6
Ismayil Khayredinov <ismayil.khayredinov@gmail.com>
2017-06-24 16:08:17 +0200

    fix(donations): donations list paginates properly again

--- views/default/campaigns/modules/donations.php
+++ views/default/campaigns/modules/donations.php
@@ -12,11 +12,14 @@ if (!$entity instanceof Campaign) {
 echo elgg_view('campaigns/modules/donation_stats', $vars);
 
 echo elgg_list_entities([
+	'list_id' => "donations-{$entity->guid}",
 	'types' => 'object',
 	'subtypes' => Donation::SUBTYPE,
 	'container_guids' => $entity->guid,
 	'limit' => 20,
 	'offset_key' => 'donations',
 	'no_results' => elgg_echo('campaigns:donations:no_results'),
+	'pagination' => true,
+	'pagination_type' => 'infinite',
 ]);
 
